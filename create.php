7703348a675b889eeffbaa9e1b3170dc5d6deff2
Samit Koyom <samitkoyom@gmail.com>
2021-09-24 02:22:36 +0700

    first commit

---
+++ create.php
@@ -0,0 +1,40 @@
+<?php
+
+// Allow permission headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database file
+require 'mongodb_config.php';
+
+// กำหนดตัวแปรเก็บชื่อ database และชื่อ collection
+$dbname = "phpeventdb";
+$collection = "users";
+
+// Database Connection
+$db = new DbManager();
+$conn = $db->getConnection();
+
+// print_r($conn);
+
+// รับค่าจาก php
+$data = json_decode(file_get_contents("php://input",true));
+
+// บันทึกข้อมูลลงใน Collection
+$insert = new MongoDB\Driver\BulkWrite();
+$insert->insert($data);
+$result = $conn->executeBulkWrite("$dbname.$collection", $insert);
+
+// ตรวจสอบว่าบันทึกสำเร็จหรือไม่
+if($result->getInsertedCount() == 1){
+    echo json_encode(
+		array("message" => "Record successfully created")
+	);
+} else {
+    echo json_encode(
+            array("message" => "Error while saving record")
+    );
+}
