e26d6bfd693576eca2be34d1151e5d5bb988089a
Samit Koyom <samitkoyom@gmail.com>
2021-10-01 04:11:25 +0700

    update after course

--- mongodb_config.php
+++ mongodb_config.php
@@ -2,20 +2,52 @@
 
 class DbManager {
 
-    // Database configuration
+    // แบบไม่มีการกำหนด user และ pass
+    // private $host = "localhost";
+    // private $port = "27017";
+
+    // แบบกำหนด username และ password
     private $host = "localhost";
     private $port = "27017";
+    private $userdb = "samit";
+    private $passdb = "123456";
+    private $dbname = "phpeventdb";
     private $conn;
 
+    // แบบเชื่อมต่อไปที่ Atlas cluster
+    // private $host = "cluster0.fi3ys.mongodb.net";
+    // private $userdb = "samit";
+    // private $passdb = "xxxx";
+    // private $conn;
+
     function __construct(){
+
+        // Connecting to MongoDB แบบไม่มี username และ password
+        // try{
+        //     $this->conn = new MongoDB\Driver\Manager('mongodb://'.$this->host.':'.$this->port);
+        // }catch(MongoDBDriverExceptionException $e){
+        //     echo $e->getMessage();
+        //     echo nl2br("n");
+        // }
         
-        // Connecting to MongoDB
+        // Connecting to MongoDB แบบมี username และ password
         try{
-            $this->conn = new MongoDB\Driver\Manager('mongodb://'.$this->host.':'.$this->port);
+            // mongodb://samit:123456@localhost:27017/phpeventdb
+            $this->conn = new MongoDB\Driver\Manager(
+                'mongodb://'.$this->userdb.':'.$this->passdb.'@'.$this->host.':'.$this->port.'/'.$this->dbname
+            );
         }catch(MongoDBDriverExceptionException $e){
             echo $e->getMessage();
             echo nl2br("n");
         }
+
+        // Connecting to MongoDB atlas
+        // try{
+        //     $this->conn = new MongoDB\Driver\Manager('mongodb+srv://'.$this->userdb.':'.$this->passdb.'@'.$this->host);
+        // }catch(MongoDBDriverExceptionException $e){
+        //     echo $e->getMessage();
+        //     echo nl2br("n");
+        // }
 
     }
 
